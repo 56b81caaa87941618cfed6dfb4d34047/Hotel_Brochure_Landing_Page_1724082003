cb064191042e8cc523c81f0acf47cff6beb9eb35
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-27 18:13:10 +0000

    Merge code from frontend

--- components/page_details_cards_component_1724082005.js
+++ components/page_details_cards_component_1724082005.js
@@ -3,23 +3,7 @@
 */
 Vue.component("page_details_cards_component_1724082005", {
     template: `
-    <section id="page-details-cards-component" class="py-8 bg-white">
-        <h2 id="details-title" class="text-lg font-semibold mb-5 pl-4">Details</h2>
-        <ul id="details-list" class="grid gap-4 min-[480px]:grid-cols-3 text-sm">
-            <li id="project-length" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900">
-                <div class="font-medium text-slate-200 text-slate-400">Elegant Design</div>
-                <div class="text-slate-400">Our brochures showcase your hotel's unique charm with stunning visuals and sophisticated layouts.</div>
-            </li>
-            <li id="start-date" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900">
-                <div class="font-medium" id="start-date-des-1 text-slate-200 text-slate-400">Customizable Templates</div>
-                <time class="text-slate-400" id="start-time-des-1">Choose from a wide range of fully customizable templates to perfectly represent your brand and amenities.</time>
-            </li>
-            <li id="end-date" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900">
-                <div class="font-medium" id="end-date-des-1 text-slate-200 text-slate-400">Effortless Creation</div>
-                <time class="text-slate-400" id="end-time-des-1">Create professional-quality hotel brochures in minutes with our user-friendly design tools and intuitive interface.</time>
-            </li>
-        </ul>
-    </section>
+    <section id="page-details-cards-component" class="py-8 bg-white" style="" data-mf-new="true"><h2 id="details-title" class="text-lg font-semibold mb-5 pl-4" style="" data-mf-new="true">aaaaaaaaaaaa</h2> <ul id="details-list" class="grid gap-4 min-[480px]:grid-cols-3 text-sm" style=""><li id="project-length" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900" style=""><div class="font-medium text-slate-200 text-slate-400">Elegant Design</div> <div class="text-slate-400" style="">Our brochures showcase your hotel's unique charm with stunning visuals and sophisticated layouts.</div></li> <li id="start-date" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900" style=""><div id="start-date-des-1 text-slate-200 text-slate-400" class="font-medium" style="">Customizable Templates</div> <time id="start-time-des-1" class="text-slate-400">Choose from a wide range of fully customizable templates to perfectly represent your brand and amenities.</time></li> <li id="end-date" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900"><div id="end-date-des-1 text-slate-200 text-slate-400" class="font-medium">Effortless Creation</div> <time id="end-time-des-1" class="text-slate-400">Create professional-quality hotel brochures in minutes with our user-friendly design tools and intuitive interface.</time></li></ul></section>
     `,
     data() {
         return {
